1db5bb28f2428dd8d6739952757a78af175f39dd
Scully87 <scully87.ts@googlemail.com>
2014-12-09 15:56:10 +0000

    first commit

---
+++ app/server.rb
@@ -0,0 +1,22 @@
+require 'sinatra'
+require 'sinatra/partial'
+
+set :partial_template_engine, :erb
+set :public_folder, Proc.new { File.join(root, '..', 'public') }
+# set :views, settings.root + './views'
+
+get '/' do
+  erb :index
+end
+
+get '/about' do
+	erb :about
+end
+
+get '/portfolio' do
+	erb :portfolio
+end
+
+get '/contact' do
+	erb :contact
+end
---
+++ public/js/interface.js
@@ -0,0 +1,7 @@
+$('p#1').focus()
+  .typetype("\n\nTIM SCULLY")
+	.fadeTo(4000,0.5)
+  
+$('p#2').focus().delay(3000)
+  .typetype("\n\nSIMPLE WEB DEVELOPMENT")
+  .fadeTo(400,0.5).delay(1000)
