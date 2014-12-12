d8ba9478403970f8754c4e7d905eac54c1ba9a76
Scully87 <scully87.ts@googlemail.com>
2014-12-12 17:43:19 +0000

    edited contact page

--- public/js/interface.js
+++ public/js/interface.js
@@ -6,10 +6,10 @@ $(document).ready(function() {
 	  
 	$('p#dev').focus().delay(2500)
 	  .typetype("\nWEB DEVELOPMENT")
-	  .fadeTo(400,0.6).delay(1000)
+	  .fadeTo(1000,0.6).delay(1000)
 
 	$('p#made').focus().delay(5000)
 	  .typetype("\nMADE SIMPLE")
-	  .fadeTo(400,0.6).delay(1000)
+	  .fadeTo(1000,0.6).delay(1000)
 });
        
